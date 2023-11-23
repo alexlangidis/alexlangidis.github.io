f1736a451caba869d3118c244e7cb04a211f8645
alexlag7 <alexlag.dev@gmail.com>
2023-11-23 14:15:56 +0200

    main

---
+++ src/script.js
@@ -0,0 +1,24 @@
+$(document).ready(function () {
+
+
+    $('.fa-bars, .fa-xmark').click(function (e) { 
+        e.preventDefault();
+        $('.fa-bars, .fa-xmark, .mobile-menu').toggleClass('hidden');
+    });
+
+
+       // Smooth scroll when li is clicked
+       $('.scroll-link').click(function (e) {
+        e.preventDefault();
+        let target = $(this.hash);
+        $('html, body').animate({
+          scrollTop: target.offset().top
+        }, 1000); // Adjust the speed of the scroll as needed
+      });
+
+ // Close mobile menu when li is clicked
+    $('.mobile-menu li').click(function () {
+      $('.fa-bars, .fa-xmark, .mobile-menu').toggleClass('hidden');
+    });
+
+});
